3ffac35270a1efe25dc924cdea3a56f9e99d513e
Lars Vogel <Lars.Vogel@vogella.com>
2016-05-29 22:52:09 +0200

    [Maven] Adds the Eclipse Maven as AsciiDoc

---
+++ EclipseMaven/res/first/App.java
@@ -0,0 +1,12 @@
+package com.vogella.maven.lars;
+
+import com.google.gson.Gson;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Gson gson = new Gson();
+        System.out.println(gson.toJson("Hello World!") );
+    }
+}
---
+++ EclipseMaven/res/first/Main.java
@@ -0,0 +1,8 @@
+package com.vogella.build.maven.simple;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Maven!");
+	}
+}

2af93a0d86c509cef4262e2a494c1283cf3cf7c6
Simon Scholz <simon.scholz@vogella.com>
2016-06-23 01:03:44 +0200

    [EclipseWizards] Added Exercise for a new Wizard of the Eclipse IDE
    Change-Id: I9a319cdedd52c1e79d1104f1885f201ba5cae93c
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- EclipseWizards/res/wizard/MyNewWizard.java
+++ EclipseWizards/res/wizard/MyNewWizard.java
@@ -5,15 +5,25 @@ import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
 
-public class MyWizard extends Wizard implements INewWizard {
+public class MyNewWizard extends Wizard implements INewWizard {
+
+	public MyNewWizard() {
+	}
 
 	// ...
 	// other MyWizard code
 	// ...
-	
+
 	@Override
 	public void init(IWorkbench workbench, IStructuredSelection selection) {
 		// initialize wizard with workbench and the current selection
-		
+
 	}
+
+	@Override
+	public boolean performFinish() {
+		return false;
+	}
+
 }
+

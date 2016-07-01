eb46e73eb42702b849effad18cc4869b741fe182
Simon Scholz <simon.scholz@vogella.com>
2016-07-01 14:28:42 +0200

    [EclipsePlugin] Updated Marker Exercise to Eclipse 4 and use adapters
    Change-Id: I0ee9e00de0ab685d16f5b4fd1b549505c7e9f061
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- EclipsePlugin/examples/marker/AddMarker.java
+++ EclipsePlugin/examples/marker/AddMarker.java
@@ -1,37 +1,40 @@
 package de.vogella.plugin.markers.handler;
 
-import org.eclipse.core.commands.AbstractHandler;
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
+import javax.inject.Named;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.core.services.adapter.Adapter;
 import org.eclipse.e4.ui.services.IServiceConstants;
+import org.eclipse.jface.viewers.IStructuredSelection;
+
 
 public class AddMarker {
 
 	@Execute
-	public void execute(@Named(IServiceConstants.ACTIVE_SELECTION) IStructuredSelection selection) {
-		if (selection == null) {
+	public void execute(@Named(IServiceConstants.ACTIVE_SELECTION) IStructuredSelection selection, Adapter adapter) {
+
+		if (selection == null && !selection.isEmpty()) {
 			return;
 		}
+
 		Object firstElement = selection.getFirstElement();
-		if (firstElement instanceof IJavaProject) {
-			IJavaProject type = (IJavaProject) firstElement;
-			writeMarkers(type);
+		IResource resource = adapter.adapt(firstElement, IResource.class);
+
+		if (resource != null) {
+			writeMarkers(resource);
 		}
+
 	}
 
-	private void writeMarkers(IJavaProject type) {
+	private void writeMarkers(IResource resource) {
 		try {
-			IResource resource = type.getUnderlyingResource();
 			IMarker marker = resource.createMarker(IMarker.TASK);
-			marker.setAttribute(IMarker.MESSAGE, "This a a task");
+			marker.setAttribute(IMarker.MESSAGE, "This is a task");
 			marker.setAttribute(IMarker.PRIORITY, IMarker.PRIORITY_HIGH);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+
 }

8fd58b71129439a944236847500c244296a6d9e3
Simon Scholz <simon.scholz@vogella.com>
2016-07-01 20:48:41 +0200

    [EclipsePlugin] Added an exercise for adapter with the WorkbenchLabelProvider
    Change-Id: I0c9ed8fccde89acabfd2b2be7b7c3f047ee99c9f
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

---
+++ EclipsePlugin/examples/adapter/TodoAdapterFactoryWorkbenchAdapter.java
@@ -0,0 +1,29 @@
+package de.vogella.plugin.adapter;
+
+import org.eclipse.core.runtime.IAdapterFactory;
+import org.eclipse.ui.model.WorkbenchAdapter;
+import org.eclipse.ui.views.properties.IPropertySource;
+
+import de.vogella.plugin.adapter.model.Todo;
+
+public class TodoAdapterFactory implements IAdapterFactory {
+
+	// use a static final field so that the adapterList is only instanciated once
+	private static final Class<?>[] adapterList = new Class<?>[] { IPropertySource.class, WorkbenchAdapter.class };
+
+	@Override
+	public <T> T getAdapter(Object adaptableObject, Class<T> adapterType) {
+		if (adapterType== IPropertySource.class && adaptableObject instanceof Todo){
+			return adapterType.cast(new TodoPropertySource((Todo) adaptableObject));
+		} else if (adapterType.isAssignableFrom(WorkbenchAdapter.class) && adaptableObject instanceof Todo) {
+			return adapterType.cast(new TodoWorkbenchAdapter());
+		}
+		return null;
+	}
+
+	@Override
+	public Class<?>[] getAdapterList() {
+		return adapterList;
+	}
+
+}

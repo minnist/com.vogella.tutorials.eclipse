9f1c1ed079eab656c76260b728932982148f1a30
Lars Vogel <Lars.Vogel@vogella.com>
2016-07-12 22:29:24 +0200

    Eclipse plug-in xml version removed

--- EclipsePlugIn/examples/context/View.java
+++
@@ -1,7 +0,0 @@
-// get the context of a part
-IEclipseContext partContext = 
-	(IEclipseContext) getViewSite().getService(IEclipseContext.class);
-
-// or access directly a value in the context based on its key
-EModelService service = 
-	(EModelService) getViewSite().getService(EModelService.class);
